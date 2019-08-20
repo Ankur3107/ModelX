22bf4548c86852c74365f60e101baa2d57f2a93d
Ankur <ankur310794@gmail.com>
2019-08-20 19:12:03 +0530

    BiLSTMGRUMultiHeadAttention

--- modelX/bilstm_architecture.py
+++ modelX/bilstm_architecture.py
@@ -2,6 +2,9 @@ from keras.layers import *
 from keras.optimizers import *
 from keras.models import Model, Sequential
 import numpy as np
+from keras_multi_head import MultiHead
+from keras_multi_head import MultiHeadAttention
+from keras_self_attention import SeqSelfAttention
 
 class BiLSTMGRUSpatialDropout1D():
     def __init__(self, nb_words, embedding_size, embedding_matrix=None, is_embedding_trainable=False, h_lstm=256, h_gru=128):
@@ -55,3 +58,30 @@ class BiLSTMGRUSelfAttention():
         conc = Concatenate()([max_pool1, max_pool2])
         return conc
 
+class BiLSTMGRUMultiHeadAttention():
+    def __init__(self, nb_words, embedding_size, embedding_matrix=None, is_embedding_trainable=False, h_lstm=256, h_gru=128):
+        
+        if embedding_matrix is None:
+            embedding_matrix = np.zeros((nb_words, embedding_size))
+
+        self.nb_words = nb_words
+        self.embedding_size = embedding_size
+        self.embedding_matrix = embedding_matrix
+        self.is_embedding_trainable = is_embedding_trainable
+        self.h_lstm = h_lstm
+        self.h_gru = h_gru
+        
+    def __call__(self,pre_layer):
+        x = Embedding(self.nb_words, self.embedding_size, weights=[self.embedding_matrix], trainable=self.is_embedding_trainable)(pre_layer)
+        x = SpatialDropout1D(0.3)(x)
+        
+        x1 = MultiHead(Bidirectional(CuDNNLSTM(self.h_lstm, return_sequences=False)),layer_num=3)(x)
+        x1_self = MultiHeadAttention(head_num=3)(x1)
+    
+        x2 = MultiHead(Bidirectional(CuDNNGRU(self.h_gru, return_sequences=False)),layer_num=3)(x)
+        x2_self = MultiHeadAttention(head_num=3)(x2)
+    
+        max_pool1 = GlobalMaxPooling1D()(x1_self)
+        max_pool2 = GlobalMaxPooling1D()(x2_self)
+        conc = Concatenate()([max_pool1, max_pool2])
+        return conc
