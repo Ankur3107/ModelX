f62d3a43557d06e97257141c40b51982d6774ece
Ankur3107 <ankur310794@gmail.com>
2020-04-12 19:13:18 +0530

    new split arch added

--- model_X/bilstm_architectures.py
+++ model_X/bilstm_architectures.py
@@ -5,6 +5,7 @@ import numpy as np
 from .utils import MultiHead
 from .utils import MultiHeadAttention
 from .utils import SeqSelfAttention
+import tensorflow as tf
 
 class BiLSTMGRUSpatialDropout1D():
     def __init__(self, nb_words, embedding_size, embedding_matrix=None, is_embedding_trainable=False, h_lstm=256, h_gru=128):
@@ -86,3 +87,59 @@ class BiLSTMGRUMultiHeadAttention():
         max_pool2 = GlobalMaxPooling1D()(x2_self)
         conc = Concatenate()([max_pool1, max_pool2])
         return conc
+
+class SplitBiLSTMGRUSpatialDropout1D():
+    def __init__(self, nb_words, embedding_size, embedding_matrix=None, is_embedding_trainable=False, h_lstm1=256, h_lstm2=512, h_gru=128):
+        
+        if embedding_matrix is None:
+            embedding_matrix = np.zeros((nb_words, embedding_size))
+
+        self.nb_words = nb_words
+        self.embedding_size = embedding_size
+        self.embedding_matrix = embedding_matrix
+        self.is_embedding_trainable = is_embedding_trainable
+        self.h_lstm1 = h_lstm1
+        self.h_lstm2 = h_lstm2
+        self.h_gru = h_gru
+        
+    def __call__(self,pre_layer):
+        x = Embedding(self.nb_words, self.embedding_size, weights=[self.embedding_matrix], trainable=self.is_embedding_trainable)(pre_layer)
+        x = SpatialDropout1D(0.3)(x)
+        splits = Lambda(lambda x: tf.split(x, num_or_size_splits=2, axis=2))(x)
+
+        x1 = Bidirectional(LSTM(self.h_lstm1, return_sequences=True))(splits[0])
+        x2 = Bidirectional(LSTM(self.h_lstm2, return_sequences=True))(splits[1])
+
+        conc = Concatenate()([x1, x2])
+        conct_layer = Bidirectional(GRU(self.h_gru, return_sequences=True))(conc)
+        max_pool_layer = GlobalMaxPooling1D()(conct_layer)
+
+        return max_pool_layer
+
+
+class SplitBiLSTMGRU():
+    def __init__(self, nb_words, embedding_size, embedding_matrix=None, is_embedding_trainable=False, h_lstm1=256, h_lstm2=512, h_gru=128):
+        
+        if embedding_matrix is None:
+            embedding_matrix = np.zeros((nb_words, embedding_size))
+
+        self.nb_words = nb_words
+        self.embedding_size = embedding_size
+        self.embedding_matrix = embedding_matrix
+        self.is_embedding_trainable = is_embedding_trainable
+        self.h_lstm1 = h_lstm1
+        self.h_lstm2 = h_lstm2
+        self.h_gru = h_gru
+        
+    def __call__(self,pre_layer):
+        x = Embedding(self.nb_words, self.embedding_size, weights=[self.embedding_matrix], trainable=self.is_embedding_trainable)(pre_layer)
+        splits = Lambda(lambda x: tf.split(x, num_or_size_splits=2, axis=2))(x)
+
+        x1 = Bidirectional(LSTM(self.h_lstm1, return_sequences=True))(splits[0])
+        x2 = Bidirectional(LSTM(self.h_lstm2, return_sequences=True))(splits[1])
+
+        conc = Concatenate()([x1, x2])
+        conct_layer = Bidirectional(GRU(self.h_gru, return_sequences=True))(conc)
+        max_pool_layer = GlobalMaxPooling1D()(conct_layer)
+
+        return max_pool_layer
