baaab6b25bfc304337ae2f9e4cd811054b35db18
Ankur3107 <ankur310794@gmail.com>
2020-04-14 21:29:22 +0530

    new version out

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
      name='model_X',  
-     version='0.1.2',
+     version='0.1.3',
      author="Ankur Singh",
      author_email="ankur310794@gmail.com",
      description="This package contains collection of models",
