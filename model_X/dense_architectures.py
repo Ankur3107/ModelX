4d0d52efd30334b7adae558f32c51ab218084ef3
Ankur3107 <ankur310794@gmail.com>
2020-06-03 20:49:53 +0530

    dense dropout layer added

--- model_X/dense_architectures.py
+++ model_X/dense_architectures.py
@@ -12,3 +12,16 @@ class DenseLayerModel():
         for i in range(len(self.hidden_layer_size)):
             pre_layer = Dense(self.hidden_layer_size[i], activation=self.activation_function)(pre_layer)
         return pre_layer
+
+
+class DenseLayerModelWithDropout():
+    def __init__(self, hidden_layer_size=[1024,512,256,128, 64], dropout=0.2, activation_function='relu'):
+        self.hidden_layer_size = hidden_layer_size
+        self.activation_function = activation_function
+        self.dropout = dropout
+    
+    def __call__(self,pre_layer):
+        for i in range(len(self.hidden_layer_size)):
+            pre_layer = Dense(self.hidden_layer_size[i], activation=self.activation_function)(pre_layer)
+            pre_layer = Dropout(self.dropout)(pre_layer)
+        return pre_layer
